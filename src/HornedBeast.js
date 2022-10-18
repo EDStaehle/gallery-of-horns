6d1f2789c5c74ed85a0789d7c50066c4b53c833c
Elias <elias.staehle@gmail.com>
2022-10-18 17:32:10 -0400

    day-two

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,22 +1,54 @@
 import React from "react";
 
-class HornedBeast extends React.Component{
-  render(){
-    return(
+import './HornedBeasts.css';
+import Card from 'react-bootstrap/Card';
+
+class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favs: '',
+      favnum: 0,
+      favstop: '',
+    };
+  };
+  handlefavs = (event) => {
+    let stop = document.querySelector(`[alt='${event.target.alt}']`)
+    this.state.favnum !== 10? this.setState({
+      favs: this.state.favs + '❤️',
+      favnum: this.state.favnum + 1,
+    })
+
+    : this.setState({
+     favstop: 'Only 10 hearts allowed',
+    })
+    if(this.state.favnum >= 10){
+      stop.removeEventListener('click', this.handlefavs);
+      console.log('hello');
+    }
+    console.log(this.handlefavs);
+  }
+  render() {
+    
+    return (
       <article>
-        <h3>{this.props.title}</h3>
-        <img 
-        title = {this.props.title}
-        src ={this.props.imgUrl} 
-        alt= {this.props.description}
-        style={{
-            resizeMode: "cover",
-            height: 100,
-            width: 200
-          }}>
-          </img>
+        <Card >
+          
+          <Card.Img variant="top"
+            onClick={this.state.favstop ?()=>{} : this.handlefavs}
+            title={this.props.title}
+            src={this.props.image_url}
+            alt={this.props.title}
+            id={this.props._id}
+            />
+            <Card.Body>
+            <p> Number of ❤️ = favs:{this.state.favs}</p>
+            <p>{this.state.favstop}</p>
+           <Card.Title><h3>{this.props.title}</h3></Card.Title>
           <p>{this.props.description}</p>
-      </article>
+          </Card.Body>
+        </Card>
+      </article >
     )
   }
 }
--- src/Main.js
+++ src/Main.js
@@ -1,26 +1,22 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
-
-class Main extends React.Component{
-  render(){
-    return(
-      <main>
+import data from "./data.json";
+import './Main.css'
+class Main extends React.Component {
+  render() {
+    let beasts = [];
+    data.forEach((animal, index) => {
+      beasts.push(
         <HornedBeast
-         title = "Horned Owl" 
-         imgUrl = "https://images.unsplash.com/photo-1469297677538-c7312a64dbe8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-        description = 'horned owl'
-         />
-        <HornedBeast 
-        title = "Horned Sheep" 
-        imgUrl = "https://modernfarmer.com/wp-content/uploads/2014/08/bighornsheep12001.jpg"
-        description = "horned sheep"
+          title={animal.title}
+          image_url={animal.image_url}
+          key={index}
         />
-        <HornedBeast 
-        title = "African Buffalo" 
-        imgUrl = "https://www.activewild.com/wp-content/uploads/2022/05/African-buffalo-cape-buffalo.jpg"
-        description = "African Buffalo"
-        />
-        
+      )
+    });
+    return (
+      <main>
+        {beasts}
       </main>
     )
   }
